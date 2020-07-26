c538611fbf60fa317f446f4456f167f8c8c96f6b
matabond <matija.ivanusa@gmail.com>
2020-07-26 14:11:11 +0200

    pl scraper v2

---
+++ README.md
@@ -0,0 +1,23 @@
+# Phone and logo scraper (plscraper)
+
+Command-line application that, given a list of website URLs as input, visits them and finds, extracts and outputs the websites' logo image URLs and all phone numbers (e.g. mobile phones, land lines, fax numbers) present on the websites.
+
+## Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```python
+cat data.txt | python3 -m plscraper
+cat data.txt | docker run -i plscraper
+
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- plscraper/plscraper.py
+++ plscraper/plscraper.py
@@ -6,8 +6,15 @@ from lxml.html import fromstring
 import requests
 import headers
 from bs4 import BeautifulSoup
+import urllib.parse
+import json
+import time
+
 
 def get_website_text(html):
+    """ This function returns clean text from the website so it is easier to 
+    extract the phone numbers.
+    """
     soup = BeautifulSoup(html, "html.parser")
     text = soup.find_all(text=True)
     output = ""
@@ -36,6 +43,13 @@ def get_website_text(html):
 
 
 def clean_phone_numbers(phone_numbers_from_href, phone_numbers_from_text):
+    """ Function which merges the phone numbers 
+    gotten from the html code and pure website text.
+    Also, light cleaning of the found phone numbers is being performed: 
+    all characters that are not digits, a plus sign (+) or parentheses 
+    with whitespace are being removed
+    Function returns distinct phone numbers
+    """
     phone_digits = []
     cleaned_phone_numbers = []
 
@@ -59,42 +73,108 @@ def clean_phone_numbers(phone_numbers_from_href, phone_numbers_from_text):
 
 
 def get_phone_numbers(html):
+    """ Function which fetches phone numbers from clean website text with 
+    regular expressions and from html code
+    """
+
     regexp = (
         "\+?\s{0,5}\(?\s{0,5}\d{1,5}\s{0,5}\(?\)?\-?\s{0,5}\d{1,4}\s{0,5}"
-        "\)?\-?\s{0,5}\d{1,4}\s{0,5}\-?\s{0,5}\d{1,4}\s{0,5}\-?\s{0,5}\d{1,4}\)?"
+        "\)?\-?\s{0,5}\d{1,4}\s{0,5}\-?\s{0,5}\d{1,4}\s{0,5}\-?\s{0,5}"
+        "\d{1,4}\)?"
     )
-    phone_numbers_from_href = re.findall(r"(?<=\=\"tel\:).*?(?=\")", html)
     phone_numbers_from_text = re.findall(
         r"" + regexp + "", get_website_text(html)
     )
+    phone_numbers_from_href = re.findall(r"(?<=\=\"tel\:).*?(?=\")", html)
+
     return clean_phone_numbers(
         phone_numbers_from_href, phone_numbers_from_text
     )
 
-def get_logo_links (html):
-    tree=fromstring(html)
-    logos=tree.xpath("//img[contains(@class,'logo')]/@src")
-    return logos
+
+def prioritize_logos(logos):
+    """ Because only one logo is asked this 
+    function prioritizes fetched logos
+    """
+    d = {}
+    for logo in logos:
+        if "small" in logo.lower():
+            d[logo] = 10
+        elif "footer" in logo.lower():
+            d[logo] = 9
+        elif "header" in logo.lower():
+            d[logo] = 8
+        elif "white" in logo.lower():
+            d[logo] = 7
+        elif "black" in logo.lower():
+            d[logo] = 6
+        elif "rgb" in logo.lower():
+            d[logo] = 5
+        elif "color" in logo.lower():
+            d[logo] = 4
+        else:
+            d[logo] = 3
+    sorted(d.items(), key=lambda x: x[1])
+    return next(iter(d))
+
+
+def get_absolute_path(url, logo_link):
+    return urllib.parse.urljoin(url, logo_link)
+
+
+def get_logo_link(html, url):
+    """ Function which fetches logo links from html code
+    First we try to fetch all img tags with class containing "logo"
+    if we didn't find any we try to search for other img tags 
+    with link containing "logo".
+    If we have found more then one logo we prioritize them and return the first
+    """
+    tree = fromstring(html)
+    images_src_logo = []
+    images_class_logo = tree.xpath("//img[contains(@class,'logo')]/@src")
+    if not images_class_logo:
+        images_src = tree.xpath("//img/@src")
+        for i in images_src:
+            if "logo" in i.lower():
+                images_src_logo.append(i)
+
+    logos = list(set(images_class_logo + images_src_logo))
+
+    if len(logos) > 1:
+        logo = prioritize_logos(logos)
+    elif len(logos) == 1:
+        logo = logos[0]
+    else:
+        logo = ""
+
+    return get_absolute_path(url, logo)
+
 
 def download_url(url, num_retries=3):
-    """ Download a given URL and return the page content"""
-    logging.info("Downloading:" + url)
+    """ Download a given URL and return the response.
+    If error occurs try again with different user_agent
+    """
     try:
         response = requests.get(url, headers=headers.random_header())
     except Exception as e:
         logging.exception("website: " + url + ", download error:" + str(e))
         if num_retries > 0:
+            time.sleep(1)
             return download_url(url, num_retries - 1)
-    logging.info("Downloaded:" + url)
     return response
 
+
 def worker(url):
-    response = download_url(url)
-    html = response.text
-    logging.info("Getting phone numbers:" + url)
+    """ Main function for fetching and cleaning data from the website """
+    html = download_url(url).text
     phone_numbers = get_phone_numbers(html)
-    logging.info(url)
-    logging.info(phone_numbers)
+    logo = get_logo_link(html, url)
+
+    result = {}
+    result["logo"] = logo
+    result["phones"] = phone_numbers
+    result["website"] = url
+    print(json.dumps(result))
 
 
 if __name__ == "__main__":
@@ -102,12 +182,10 @@ if __name__ == "__main__":
         "%(asctime)s::%(threadName)-10s::%(levelname)s::%(name)s::"
         "%(lineno)d::%(message)s"
     )
-    logging.basicConfig(filename="log.txt", level="INFO", format=log_format)
+    logging.basicConfig(stream=sys.stderr, level="INFO", format=log_format)
     urls = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         futures = []
         for url in urls:
             futures.append(executor.submit(worker, url.strip()))
-        for future in concurrent.futures.as_completed(futures):
-            pass
